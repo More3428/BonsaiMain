3db7ac92fc02cb2a47fcb9cd04a144f9dba9f28b
Andre Moreno <andre.moreno5270@gmail.com>
2025-05-19 18:48:05 -0700

    Created bonsai class and called display bonsais.
    needs testing
    Display is not done yet.

--- src/bonsai.cpp
+++ src/bonsai.cpp
@@ -1,3 +1,46 @@
 /*
  Implementation of bonsai logic
 */
+#include <iostream>
+#include "bonsai.h"
+#include <vector>
+
+
+
+
+Bonsai createBonsai(){
+    std::string name;
+    std::string species;
+    int age;
+    std::string lastWatered;
+    std::string lastPruned;
+    std::string notes;
+
+    std::cout << "Enter the name of your Bonsai." << std::endl;
+    std::getline(std::cin,name);
+
+    std::cout << "Enter the species of your tree." << std::endl;
+    std::getline(std::cin,species);
+
+    std::cout << "Enter the approx. age of your bonsai." << std::endl;
+    std::cin >> age;
+    std::cin.ignore();
+
+    std::cout << "Enter the date of last time watered." << std::endl;
+    std::getline(std::cin,lastWatered);
+    
+    std::cout << "Enter the last time you pruned your tree." << std::endl;
+    std::getline(std::cin,lastPruned);
+    
+    std::cout << "Add any additional notes." << std::endl;
+    std::getline(std::cin,notes);
+    
+    return Bonsai{name, species, age, lastWatered, lastPruned, notes};
+
+}
+
+void displayBonsai(const Bonsai& b) {
+    
+    
+
+}
